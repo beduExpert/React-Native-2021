4ad1c8bc2dcda9cc7eaa1519acba734f8440260c
Santi Siordia <santiago.gonzalez.siordia@gmail.com>
2021-11-17 16:59:12 -0600

    feat: last 4 componentes added

---
+++ Sesion-01/Ejemplo-03/View.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const ViewBoxesWithColorAndText = () => {
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        height: 100,
+        padding: 20
+      }}
+    >
+      <View style={{ backgroundColor: "blue", flex: 0.3 }} />
+      <View style={{ backgroundColor: "red", flex: 0.5 }} />
+      <Text>Hello World!</Text>
+    </View>
+  );
+};
+
+export default ViewBoxesWithColorAndText;
